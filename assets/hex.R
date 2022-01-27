d0555064c7f6c815fd51d666d2f49051581db63e
Roger Beecham <r.j.beecham@leeds.ac.uk>
2022-01-27 14:29:40 +0000

    2022 updates

--- assets/hex.R
+++ assets/hex.R
@@ -9,7 +9,8 @@ p <- d %>% ggplot()+
 s <- sticker(p, package="GEOG5927M",
         p_size=4, s_x=1, s_y=0.9, s_width = 5, s_height = 5,
         h_color="#ffffff",
-        h_fill="#a50f15",
+        #h_fill="#a50f15",
+        h_fill="#252525",
         p_family="Avenir Book",
         u_family="Avenir Book")
 ggsave(s, filename="./static/images/geog5927m2.png", width=2, height=2, dpi=300)
